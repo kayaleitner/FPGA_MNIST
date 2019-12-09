9b3b98005035f33f5fb3dcc45f56a2278177de79
taq0 <taq0@outlook.com>
2019-12-09 17:40:21 +0100

    removed socket files (not needed anymore)

--- src/c/main.c
+++ src/c/main.c
@@ -1,17 +1,6 @@
-#include "client.h"
 #include "dbg.h"
 
 int main() 
 {
-    log_info("Starting client");
-
-    //int sockfd_data = open_connection(DATA_PORT);
-    int sockfd_control = open_connection(CONTROL_PORT);
-
-
-    write(sockfd_control, "Hello", strlen("Hello"));     
-
-    //close(sockfd_data); 
-    close(sockfd_control);
-    log_info("Closing Socket. End");
+    return ;
 } 
