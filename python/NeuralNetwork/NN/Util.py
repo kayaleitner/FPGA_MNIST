bb553393aa90d6c92dc57e36206ece0d29c58a55
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2019-12-21 00:46:57 +0100

    Added channel order conversation function

--- python/NeuralNetwork/NN/Util.py
+++ python/NeuralNetwork/NN/Util.py
@@ -1,6 +1,5 @@
 import numpy as np
 from numpy import reshape
-from numpy.core.multiarray import ndarray
 from NeuralNetwork.NN.Layer import Layer
 
 
@@ -19,8 +18,38 @@ def ind2sub(ind, shape):
     return tuple(s)
 
 
+def channels_last_2_channels_first(x: np.ndarray):
+    """
+    Converts a tensor with shape [B,H,W,C] to a tensor with shape [B,C,H,W]
+    ToDo: Definitely needs a test
+    """
+    B, H, W, C = x.shape
+    y = np.zeros(shape=(B, C, H, W))
+    for b in range(B):
+        for h in range(H):
+            for w in range(W):
+                for c in range(C):
+                    # ToDo: Vectorize without changing behaviour or rewrite in C for performance
+                    y[b, c, h, w] = x[b, h, w, c]
+
+
+def channels_first_2_channels_last(x: np.ndarray):
+    """
+    Converts a tensor with shape [B,C,H,W] to a tensor with shape [B,H,W,C]
+    ToDo: Definitely needs a test
+    """
+    B, C, H, W = x.shape
+    y = np.zeros(shape=(B, H, W, C))
+    for b in range(B):
+        for h in range(H):
+            for w in range(W):
+                for c in range(C):
+                    # ToDo: Vectorize without changing behaviour or rewrite in C for performance
+                    y[b, h, w, c] = x[b, c, h, w]
+
+
 class ReshapeLayer(Layer):
-    newshape: ndarray
+    newshape: np.ndarray
 
     def __init__(self, newshape):
         self.newshape = newshape
@@ -32,5 +61,5 @@ class ReshapeLayer(Layer):
     def get_input_shape(self):
         pass  # can be anything
 
-    def get_output_shape(self, input_data_shape: ndarray = None):
+    def get_output_shape(self, input_data_shape: np.ndarray = None):
         return self.newshape
