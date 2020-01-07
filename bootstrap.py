dc735b99e781d4e1cf89f9da86eae3bbf375e61a
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-01-07 17:19:40 +0100

    Updated bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -62,11 +62,12 @@ if __name__ == "__main__":
         system('pip install virtualenv')
         system('virtualenv venv')
 
-        # Not sure if this works
-        if platform.system == 'Windows':
-            system(r'.\venv\bin\activate.bat')
-        else:
-            system('source ./venv/bin/activate')
+        # # Not sure if this works
+        # if platform.system == 'Windows':
+        #     system(r'.\venv\bin\activate.bat')
+        # else:
+        #     system('source ./venv/bin/activate')
+        
         system('venv/bin/pip install -r python/requirements.txt')
     
     if not os.path.exists(join(PROJECT_DIRECTORY, 'lib', 'float')):
@@ -74,7 +75,6 @@ if __name__ == "__main__":
         URL_V_FLOAT = 'http://www.coe.neu.edu/Research/rcl/projects/floatingpoint/VFLOAT_May_2015.tar'
         (file_path, http_response) = urllib.request.urlretrieve(URL_V_FLOAT, join(PROJECT_DIRECTORY, 'lib', 'vfloat', 'VFLOAT_May_2015.tar'))
         shutil.unpack_archive(filename=file_path, extract_dir=join(PROJECT_DIRECTORY, "lib", "vfloat"))
-        
         # Alternative would be WGET but it is not default on most systems
         # system('wget -P lib/vfloat http://www.coe.neu.edu/Research/rcl/projects/floatingpoint/VFLOAT_May_2015.tar')
 
