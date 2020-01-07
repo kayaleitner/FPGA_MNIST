9302c1a7c5b0519f3cea38c39f09c75ebee8a1ee
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-01-07 19:19:06 +0100

    Fixed wrong method, update, version check

--- bootstrap.py
+++ bootstrap.py
@@ -2,24 +2,31 @@
 """
 
 
-import sys, os
+import sys
+import os
 from os.path import join
 import platform
 import tarfile
 import urllib.request
 import shutil
 
-def system(command : str, convert_slashes=False):
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
+
+
+def system(command: str, convert_slashes=False):
     if convert_slashes and platform.system() == "Windows":
         command = command.replace('/', os.path.sep)
-        
+
     code = os.system(command=command)
     if code != 0:
-        raise Exception("'{}' returned with exit code {}".format(command, code))
+        raise Exception(
+            "'{}' returned with exit code {}".format(command, code))
 
-def path_convert(path : str):
+
+def path_convert(path: str):
     # this makes sure the path is always in the correct format
-    return path.convert('/', os.path.sep)
+    return path.replace('/', os.path.sep)
 
 
 def check_if_package_manager_is_available():
@@ -35,6 +42,7 @@ def check_if_package_manager_is_available():
         except:
             raise Exception("Please install the 'Choco' package manager")
 
+
 class PackageManager:
     def __init__(self):
         super().__init__()
@@ -46,20 +54,19 @@ class PackageManager:
             self.package_command = 'sudo apt-get'
 
     def install(self, *packages):
-        command = self.package_command +  ' '.join(packages)
+        command = self.package_command + ' '.join(packages)
         system(command=command)
 
 
 if __name__ == "__main__":
-    
-    
+
     # Check package manager
     check_if_package_manager_is_available()
-    
+
     # Make sure everything is done in the right directory
     PROJECT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
     os.chdir(PROJECT_DIRECTORY)
-    
+
     # Setup consts
     LIB_VFLOAT_PATH = join(PROJECT_DIRECTORY, 'lib', 'float')
     LIB_VFLOAT_URL = 'http://www.coe.neu.edu/Research/rcl/projects/floatingpoint/VFLOAT_May_2015.tar'
@@ -67,7 +74,7 @@ if __name__ == "__main__":
     # Setup Virtualenv
     # https://stackoverflow.com/questions/1871549/determine-if-python-is-running-inside-virtualenv?noredirect=1
     if not hasattr(sys, 'real_prefix') and not os.path.exists(join(PROJECT_DIRECTORY, 'venv')):
-        
+
         # update pip, install virtualenv and create a new one
         system('python -m pip install --upgrade pip')
         system('pip install virtualenv')
@@ -78,16 +85,17 @@ if __name__ == "__main__":
         #     system(r'.\venv\bin\activate.bat')
         # else:
         #     system('source ./venv/bin/activate')
-        system('./venv/bin/pip install -r python/requirements.txt', convert_slashes=True)
+        system('./venv/bin/pip install -r python/requirements.txt',
+               convert_slashes=True)
 
-    
     if not os.path.exists(LIB_VFLOAT_PATH):
-        os.makedirs(LIB_VFLOAT_PATH, exist_ok=True) 
-        (file_path, http_response) = urllib.request.urlretrieve(LIB_VFLOAT_URL, join(PROJECT_DIRECTORY, 'lib', 'vfloat', 'VFLOAT_May_2015.tar'))
-        shutil.unpack_archive(filename=file_path, extract_dir=join(PROJECT_DIRECTORY, "lib", "vfloat"))
+        os.makedirs(LIB_VFLOAT_PATH, exist_ok=True)
+        (file_path, http_response) = urllib.request.urlretrieve(LIB_VFLOAT_URL,
+                                                                join(PROJECT_DIRECTORY, 'lib', 'vfloat', 'VFLOAT_May_2015.tar'))
+        shutil.unpack_archive(filename=file_path, extract_dir=join(
+            PROJECT_DIRECTORY, "lib", "vfloat"))
         # Alternative would be WGET but it is not default on most systems
         # system('wget -P lib/vfloat http://www.coe.neu.edu/Research/rcl/projects/floatingpoint/VFLOAT_May_2015.tar')
-
 
     # Install packages
     manager = PackageManager()
