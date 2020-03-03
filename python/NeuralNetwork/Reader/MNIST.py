41f2d9d0c741070af504ab707be0f70826fa9c08
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-03-03 11:26:50 +0100

    Added Better Python Lib

--- python/NeuralNetwork/Reader/MNIST.py
+++ python/NeuralNetwork/Reader/MNIST.py
@@ -2,7 +2,13 @@ from NeuralNetwork.Reader.Mnist2Numpy import MnistDataReader, MnistDataDownloade
 import idx2numpy
 import gzip
 
+
 class Dataset:
+    pass
+
+
+class MNIST(Dataset):
+    _classnames = map(str, [i for i in range(10)])
 
     def __init__(self, folder_path=None):
         self.downloader = MnistDataDownloader(folder_path=folder_path)
@@ -15,8 +21,10 @@ class Dataset:
         imgs_path, lbls_path = self.downloader.get_path(DataSetType.TRAIN)
         return idx2numpy.convert_from_file(gzip.open(filename=imgs_path))
 
+    def test_labels(self):
+        imgs_path, lbls_path = self.downloader.get_path(DataSetType.TEST)
+        return idx2numpy.convert_from_file(gzip.open(filename=lbls_path))
 
-class MNIST(Dataset):
-    _classnames = map(str, [i for i in range(10)])
-
-
+    def test_images(self):
+        imgs_path, lbls_path = self.downloader.get_path(DataSetType.TEST)
+        return idx2numpy.convert_from_file(gzip.open(filename=imgs_path))
