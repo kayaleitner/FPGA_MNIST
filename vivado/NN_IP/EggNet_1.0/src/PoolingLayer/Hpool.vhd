a33de5af66f999c0be02f9beab0b7420cc8c19ef
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-05-06 18:08:01 +0200

    Updated HPool

--- vivado/NN_IP/EggNet_1.0/src/PoolingLayer/Hpool.vhd
+++ vivado/NN_IP/EggNet_1.0/src/PoolingLayer/Hpool.vhd
@@ -4,49 +4,83 @@ use ieee.numeric_std.all;
 
 use work.EggNetCommon.all;
 
+-- HPool Entity
+--
 entity Hpool is
     generic
     (
-        N_CHANNELS : NATURAL := 1;      -- number of input channels
-        DATA_WIDTH : NATURAL := 8;      -- data with of each output channel 
-        POOL_H_WIDTH : NATURAL := 2     -- width of the pooling layer
+        ACTIVATION_WIDTH_BITS : natural;      -- bit width of the input
+        N_CHANNELS            : natural := 1; -- number of input channels
+
+        -- #TODO More generic implementation with more than one channel
+        -- POOL_H_WIDTH : natural := 2  -- width of the pooling layer
     );
     port
     (
         clk_i : in std_logic;
-        reset_i : in std_logic;
+        rst_i : in std_logic;
 
         valid_i : in std_logic;
         valid_o : out std_logic;
 
-        x_i : in U_CHANNEL_ARRAY(0 to N_CHANNELS-1)(DATA_WIDTH-1 downto 0);
-        y_i : in U_CHANNEL_ARRAY(0 to N_CHANNELS-1)(DATA_WIDTH-1 downto 0)
+        x_i : in U_CHANNEL_ARRAY(0 to N_CHANNELS - 1)(ACTIVATION_WIDTH_BITS - 1 downto 0);
+        y_o : out U_CHANNEL_ARRAY(0 to N_CHANNELS - 1)(ACTIVATION_WIDTH_BITS - 1 downto 0)
     );
 end entity Hpool;
 
 architecture rtl of Hpool is
 
-    type STATE is (WAITING, FULL);
-        
+    -- Types
+    type buffer_state_t is (EMPTY, FULL);
+    type buffer_values_t is array (0 to N_CHANNELS - 1) of unsigned(ACTIVATION_WIDTH_BITS - 1 downto 0);
 
-    type input_buffer_t is array (0 to N_CHANNELS-1, 0 to POOL_H_WIDTH-1) of unsigned(DATA_WIDTH-1 downto 0);
-    signal input_buffer : input_buffer_t := (others => '0');
-    signal input_buffer_count : NATURAL := 0;
-    constant ZERO_BUFFER : input_buffer_t := (others => '0');
+    -- Constants
+    constant ZERO_BUFFER      : buffer_values_t := (others => '0');
+    constant UNDEFINED_OUTPUT : buffer_values_t := (others => 'X');
+
+    -- Signals
+    signal buffer_values : buffer_values_t := (others => '0');
+    signal buffer_state  : buffer_state_t  := EMPTY;
 
 begin
 
-    process (clk_i, reset_i)
+    process (clk_i, rst_i)
     begin
         if rst_i = '1' then
-            input_buffer <= ZERO_BUFFER;
+            -- Reset to all zeros
+            buffer_state  <= EMPTY;
+            buffer_values <= ZERO_BUFFER;
+            valid_o       <= '0';
+            y_o           <= UNDEFINED_OUTPUT;
         elsif rising_edge(clk_i) then
-            for c in 0 to N_CHANNELS-1 loop
-                for i in 0 to POOL_H_WIDTH-2 loop
-                    input_buffer(c,i+1) <= input_buffer(c,i);
-                end loop;
-            end loop;
 
+            -- If valid_i is '1' then add it
+            if valid_i = '0' then
+                valid_o <= '0';
+                y_o     <= UNDEFINED_OUTPUT;
+
+                -- Reset the state, because the 
+                buffer_state <= EMPTY;
+            else
+                if buffer_state = EMPTY then
+                    -- Current state is empty, so store the input in the buffer
+                    buffer_values <= x_i;
+                    buffer_state  <= FULL;
+                    valid_o       <= '0';
+                else
+                    -- If there are already values, then output the maximum value
+                    loop_pool : for c in 0 to N_CHANNELS - 1 loop
+                        if buffer_values(c) > x_i(c) then
+                            y_o(c) <= buffer_values(c);
+                        else
+                            y_o(c) <= x_i(c);
+                        end if;
+
+                        valid_o      <= '1';
+                        buffer_state <= EMPTY;
+                    end loop; -- loop_pool
+                end if;
+            end if;
         end if;
     end process;
 end architecture;
