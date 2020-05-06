13b7ad538e38685b233db9ddeb65b4bcf3f051bb
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-05-06 22:49:20 +0200

    Updated HPool + Testbench

--- vivado/NN_IP/EggNet_1.0/sim/PoolingLayer/tb_Hpool.vhd
+++ vivado/NN_IP/EggNet_1.0/sim/PoolingLayer/tb_Hpool.vhd
@@ -9,20 +9,126 @@ context vunit_lib.vunit_context;
 
 entity tb_hpool is
     generic (
-        runner_cfg : string;
-        tb_path    : string
+        runner_cfg            : string;
+        tb_path               : string;
+        ACTIVATION_WIDTH_BITS : natural := 15;
+        N_CHANNELS            : natural := 4;
+        TB_PERIOD             : time    := 10 ns
     );
 end entity tb_Hpool;
 
 architecture rtl of tb_hpool is
 
+    -- Signals for UUT
+    signal clk      : std_logic                                            := '0';
+    signal sim_done : std_logic                                            := '0';
+    signal rst      : std_logic                                            := '0';
+    signal valid_i  : std_logic                                            := '0';
+    signal valid_o  : std_logic                                            := '0';
+    signal x_i      : std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0) := (others => '0');
+    signal y_o      : std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0) := (others => '0');
+
+    -- Variables defined for VUNIT
+    -- shared variable input_data, ref_output_data : integer_array_t;
+    -- signal start, data_check_done, stimuli_done : boolean := false;
+
+    -- Expected input type
+    type test_input_sample is record
+        valid_i : std_logic;
+        x_i     : integer;
+    end record;
+
+    -- Expected output type
+    type test_output_sample is record
+        valid_o : std_logic;
+        y_o     : integer;
+    end record;
+
+    type test_input_vector_t is array (natural range <>) of test_input_sample;
+    type test_output_vector_t is array (natural range <>) of test_output_sample;
+
+    constant test_input_vector : test_input_vector_t := (
+    ('1', 1), -- Test 1
+    ('1', 2),
+    ('1', 3), -- Test 2
+    ('1', 4),
+    ('1', 4), -- Test 3
+    ('0', 2),
+    ('0', 2), -- Test 4
+    ('0', 2),
+    ('1', 9), -- Test 5
+    ('1', 2)
+    );
+
+    constant test_output_vector : test_output_vector_t := (
+    ('0', 0), -- Test 1
+    ('1', 2),
+    ('0', 0), -- Test 2
+    ('1', 4),
+    ('0', 0), -- Test 3
+    ('0', 0),
+    ('0', 0), -- Test 4
+    ('0', 0),
+    ('0', 0), -- Test 5
+    ('1', 9)
+    );
 begin
 
-    begin
-	    -- Init VUNIT
-        test_runner_setup(runner, runner_cfg);
+    clk <= not clk after TB_PERIOD/2 when sim_done /= '1' else
+        '0';
 
+    test_runner : process
+    begin
+        test_runner_setup(runner, runner_cfg);
+        sim_done <= '0';
+        rst      <= '1';
+        wait for TB_PERIOD;
+
+        -- Read the data files
+        -- input_data      := load_csv(tb_path & "input.csv");
+        -- ref_output_data := load_csv(tb_path & "output.csv");
+
+        rst <= '0';
+        wait for TB_PERIOD;
+
+        for i in test_output_vector'range loop
+
+            -- info("Iteration: " & i'image);
+            --info("Iteration");
+            debug("Iteration");
+
+            -- Set inputs
+            wait until falling_edge(clk);
+            valid_i <= test_input_vector(i).valid_i;
+            x_i     <= std_logic_vector(to_unsigned(test_input_vector(i).x_i, ACTIVATION_WIDTH_BITS));
+
+            -- Check output
+            wait until rising_edge(clk);
+            wait for TB_PERIOD/16;
+            check_equal(valid_o, test_output_vector(i).valid_o);
+            if valid_o = '1' then
+                check_equal(unsigned(y_o), test_output_vector(i).y_o);
+            end if;
+
+        end loop;
+
+        sim_done <= '1';
         test_runner_cleanup(runner);
-	end process;
+    end process; -- test_runner
+
+    -- 
+    -- Instantiate the HPool Entity
+    hpool_0 : entity work.HPool
+        generic map(
+            ACTIVATION_WIDTH_BITS => ACTIVATION_WIDTH_BITS
+        )
+        port map(
+            clk_i   => clk,
+            rst_i   => rst,
+            valid_i => valid_i,
+            valid_o => valid_o,
+            x_i     => x_i,
+            y_o     => y_o
+        );
 
 end architecture;
--- vivado/NN_IP/EggNet_1.0/src/PoolingLayer/Hpool.vhd
+++ vivado/NN_IP/EggNet_1.0/src/PoolingLayer/Hpool.vhd
@@ -1,46 +1,39 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 use work.EggNetCommon.all;
-
 -- HPool Entity
 --
-entity Hpool is
-    generic
-    (
-        ACTIVATION_WIDTH_BITS : natural;      -- bit width of the input
-        N_CHANNELS            : natural := 1; -- number of input channels
-
+entity HPool is
+    generic (
+        ACTIVATION_WIDTH_BITS : natural -- bit width of the input
         -- #TODO More generic implementation with more than one channel
-        -- POOL_H_WIDTH : natural := 2  -- width of the pooling layer
+        -- POOL_KERNEL_SIZE : natural := 2  -- width of the pooling layer
     );
-    port
-    (
-        clk_i : in std_logic;
-        rst_i : in std_logic;
-
+    port (
+        clk_i   : in std_logic;
+        rst_i   : in std_logic;
         valid_i : in std_logic;
         valid_o : out std_logic;
-
-        x_i : in U_CHANNEL_ARRAY(0 to N_CHANNELS - 1)(ACTIVATION_WIDTH_BITS - 1 downto 0);
-        y_o : out U_CHANNEL_ARRAY(0 to N_CHANNELS - 1)(ACTIVATION_WIDTH_BITS - 1 downto 0)
+        x_i     : in std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0);
+        y_o     : out std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0)
     );
-end entity Hpool;
-
-architecture rtl of Hpool is
+end entity HPool;
+architecture rtl of HPool is
 
     -- Types
+    type buffer_data_type is array (integer range <>) of signed (ACTIVATION_WIDTH_BITS - 1 downto 0);
     type buffer_state_t is (EMPTY, FULL);
-    type buffer_values_t is array (0 to N_CHANNELS - 1) of unsigned(ACTIVATION_WIDTH_BITS - 1 downto 0);
-
     -- Constants
-    constant ZERO_BUFFER      : buffer_values_t := (others => '0');
-    constant UNDEFINED_OUTPUT : buffer_values_t := (others => 'X');
-
+    constant ZERO_BUFFER      : std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0) := (others => '0');
+    constant UNDEFINED_OUTPUT : std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0) := (others => '0');
     -- Signals
-    signal buffer_values : buffer_values_t := (others => '0');
-    signal buffer_state  : buffer_state_t  := EMPTY;
+    signal buffer_state : buffer_state_t                                       := EMPTY;
+    signal buffer_value : std_logic_vector(ACTIVATION_WIDTH_BITS - 1 downto 0) := (others => '0');
+
+    -- For larger kernel sizes than 2
+    -- signal max_value_signal : signed(ACTIVATION_WIDTH_BITS-1 downto 0);
+    -- signal buffer_data      : buffer_data_type (POOL_KERNEL_SIZE - 1 downto 0);
 
 begin
 
@@ -48,37 +41,34 @@ begin
     begin
         if rst_i = '1' then
             -- Reset to all zeros
-            buffer_state  <= EMPTY;
-            buffer_values <= ZERO_BUFFER;
-            valid_o       <= '0';
-            y_o           <= UNDEFINED_OUTPUT;
+            buffer_state <= EMPTY;
+            buffer_value <= ZERO_BUFFER;
+            valid_o      <= '0';
+            y_o          <= UNDEFINED_OUTPUT;
         elsif rising_edge(clk_i) then
 
             -- If valid_i is '1' then add it
             if valid_i = '0' then
                 valid_o <= '0';
                 y_o     <= UNDEFINED_OUTPUT;
-
                 -- Reset the state, because the 
                 buffer_state <= EMPTY;
             else
                 if buffer_state = EMPTY then
                     -- Current state is empty, so store the input in the buffer
-                    buffer_values <= x_i;
-                    buffer_state  <= FULL;
-                    valid_o       <= '0';
+                    buffer_value <= x_i;
+                    buffer_state <= FULL;
+                    valid_o      <= '0';
                 else
                     -- If there are already values, then output the maximum value
-                    loop_pool : for c in 0 to N_CHANNELS - 1 loop
-                        if buffer_values(c) > x_i(c) then
-                            y_o(c) <= buffer_values(c);
-                        else
-                            y_o(c) <= x_i(c);
-                        end if;
+                    if unsigned(buffer_value) > unsigned(x_i) then
+                        y_o <= buffer_value;
+                    else
+                        y_o <= x_i;
+                    end if;
 
-                        valid_o      <= '1';
-                        buffer_state <= EMPTY;
-                    end loop; -- loop_pool
+                    valid_o      <= '1';
+                    buffer_state <= EMPTY;
                 end if;
             end if;
         end if;
